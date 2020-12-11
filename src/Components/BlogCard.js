1001b52a4f1b59e3b86c50c4ed5f012cd39580e4
StevenWuTG <stevenwu91@icloud.com>
2020-12-10 21:15:13 -0500

    added counter

--- src/Components/BlogCard.js
+++ src/Components/BlogCard.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function BlogCard(props){
-    return (
-        <div>
-            <h5>{props.blogObject.title}</h5>
-            <img alt ="Blog" style={{maxWidth:"70vw", maxHeight: "20vh"}} src={props.blogObject.image}></img>
-            <button>Save</button>
+
+class BlogCard extends Component{
+
+    //pre-ES6 /initialize
+    // constructor(props){
+    //     super(props)
+    //     this.state = {}
+    // }
+
+    //ES6
+    state = {
+        counter : 0
+    }
+
+    incrementCounter = () => {
+        // this.state.counter = this.state.counter +1
+        this.setState({counter: this.state.counter + 1})
+        
+    }
+
+    render(){
+        const {blogObject} = this.props
+        return(
+            <div>
+            <h5>{blogObject.title}</h5>
+            <img alt ="Blog" style={{maxWidth:"70vw", maxHeight: "20vh"}} src={blogObject.image}></img>
+            <h4>Counter: {this.state.counter}</h4>
+            <button onClick={this.incrementCounter}>Save</button>
             <button>Visit</button>
-        </div>
-
-    )
+        </div>    
+        )
+    }
 }
+
+// function BlogCard(props){
+//     const { blogObject} = props
+//     return (
+//         <div>
+//             <h5>{blogObject.title}</h5>
+//             <img alt ="Blog" style={{maxWidth:"70vw", maxHeight: "20vh"}} src={props.blogObject.image}></img>
+//             <h4>Counter: 0</h4>
+//             <button>Save</button>
+//             <button>Visit</button>
+//         </div>
+
+//     )
+// }
 
 
 export default BlogCard
